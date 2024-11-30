841a229545ea7f42ad0ecbb2eac4ea875e315c6e
Kris <emineo@abv.bg>
2024-11-30 11:56:07 +0200

    add requirements & file upload func

--- data_collector.py
+++ data_collector.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import pandas as pd
+from access_vault import service_client
 
 stocks = ['META', 'AAPL', 'AMZN', 'NFLX','SAP', 'GOOG', 'NVDA']
 
@@ -12,3 +13,9 @@ df = pd.DataFrame(raw_stocks)
 
 df.to_csv('raw_stocks.csv')
 
+def upload_file(container_name:str, local_file_path:str, destination_path:str):
+     file_system_client = service_client.get_file_system_client(file_system=container_name)
+     file_client = file_system_client.get_file_client(destination_path)
+
+     with open(local_file_path, 'rb') as file:
+          file_client.upload_data(file, overwrite=True)

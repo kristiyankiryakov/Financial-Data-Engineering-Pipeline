823c3006f3568a3b801d6bbdaf64a197e057dd2d
Kris <emineo@abv.bg>
2024-11-30 12:40:16 +0200

    add jenkins automation & upload

--- data_collector.py
+++ data_collector.py
@@ -12,10 +12,3 @@ raw_stocks = collect_raw_stock_data(stocks, '6mo')
 df = pd.DataFrame(raw_stocks)
 
 df.to_csv('raw_stocks.csv')
-
-def upload_file(container_name:str, local_file_path:str, destination_path:str):
-     file_system_client = service_client.get_file_system_client(file_system=container_name)
-     file_client = file_system_client.get_file_client(destination_path)
-
-     with open(local_file_path, 'rb') as file:
-          file_client.upload_data(file, overwrite=True)

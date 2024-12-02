9b8d475d97ef890069141728a81f3328393e7e74
Kris <emineo@abv.bg>
2024-12-02 22:16:32 +0200

    refactor ingestion & add transform layer

--- data_collector.py
+++ data_collector.py
@@ -1,13 +1,36 @@
 import yfinance as yf
 import pandas as pd
-
-stocks = ['META', 'AAPL', 'AMZN', 'NFLX','SAP', 'GOOG', 'NVDA']
+import logging
 
 def collect_raw_stock_data(stocks, period):
-     return yf.download(stocks, period=period, group_by='ticker')
+     data = yf.download(stocks, period=period, group_by='ticker')
+     data = data.stack(level=0,future_stack=True).reset_index()
+     return pd.DataFrame(data)
 
-raw_stocks = collect_raw_stock_data(stocks, '6mo')
+def fix_columns(df):
+     df.columns.name = ''
+     df.columns = df.columns.str.lower().str.replace(' ','_')
 
-df = pd.DataFrame(raw_stocks)
 
-df.to_csv('raw_stocks.csv')
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    
+    stocks = ['META', 'AAPL', 'AMZN', 'NFLX','SAP', 'GOOG', 'NVDA']
+
+
+    try:
+        df = collect_raw_stock_data(stocks, '2y')
+        fix_columns(df)
+
+        df.to_csv('raw_stocks.csv', index=False)
+
+      
+    except Exception as e:
+        logging.error(f"Collecting raw data failed: {str(e)}")
+        raise
+    
+
+
+if __name__ == "__main__":
+    main()
